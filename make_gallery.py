2d73b8e08d21c89893fea1926a1aa6a989e132db
Alexander Vlasov <alexander.vlasov@smddev.com>
2015-07-20 18:37:26 +0300

    add files

---
+++ make_gallery.py
@@ -0,0 +1,16 @@
+import sys
+import json
+import pystache
+
+name = sys.argv[1]
+f1 = sys.argv[2]
+f2 = f1[0:len(f1)-5] + '.html'
+
+ff1 = open('gallery.mustache','r')
+templ = ff1.read()
+ff1.close()
+res = pystache.render(templ, {'name': name, 'images': json.load(open(f1, 'r')) })
+ff2 = open(f2,'w')
+ff2.write(res)
+ff2.close()
+
