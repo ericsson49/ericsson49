d4889d8e18cef8df856dfd2aa7ba039749ac2b60
Alexander Vlasov <alexander.vlasov@smddev.com>
2015-09-11 18:04:12 +0300

    site code reorganization

--- make_site.py
+++ make_site.py
@@ -1,26 +1,46 @@
 import sys
 import json
 import pystache
-
+import os.path
 
 with open('site.json', 'r') as f:
   site = json.load(f)
 
-port = site['galleries'][1:]
-port_names = [g['name'] for g in port]
+pages = site['pages']
+portfolio = [g for g in pages if g['type'] == 'gallery']
+port_names = [g['name'] for g in portfolio]
 
-for g in site['galleries']:
-  print g
-  with open(g['link']+'.json','r') as f:
-    ctx = site.copy()
-    ctx.update({'gen': 1, 'name': g['name'], 'images': json.load(f)})
-    menu = [{'name': g1['name'], 'link': g1['link'] + '.html', 'active': g1['link'] == g['link']} for g1 in port]
-    ctx.update({'items': [
-        {'link': 'main.html', 'name': 'Home', 'active': g['link'] == 'main'},
-        {'dropdown': 1, 'items': menu, 'name': 'Portfolio', 'active': g['name'] in port_names},
-        {'link': 'clients.html', 'name': 'Clients', 'active': g['link'] == 'clients'},
-        {'link': 'contacts.html', 'name': 'Contacts', 'active': g['link'] == 'contatcs'}
-    ]})
-    pg = pystache.Renderer(file_encoding='UTF8').render('{{> site}}', ctx)
-  with open(g['link']+'.html','w') as f:
+for p in pages:
+  print p
+  ctx = site.copy()
+
+  fname = p['link']+'.json'
+  if os.path.exists(fname) and os.path.isfile(fname):
+    with open(fname,'r') as f:
+      ctx.update({'gen': 1, 'images': json.load(f)})
+
+  menu = [{'name': g['name'], 'link': g['link'] + '.html', 'active': g['link'] == p['link']} for g in portfolio]
+  ctx.update({'items': [
+      {'link': 'main.html', 'name': 'Home', 'active': p['link'] == 'main'},
+      {'dropdown': 1, 'items': menu, 'name': 'Portfolio', 'active': p['name'] in port_names},
+      {'link': 'clients.html', 'name': 'Clients', 'active': p['link'] == 'clients'},
+      {'link': 'contacts.html', 'name': 'Contacts', 'active': p['link'] == 'contacts'}
+  ]})
+
+  if p['type'] == 'gallery' or p['type'] == 'main':
+    content = pystache.Renderer(file_encoding='UTF8').render('''
+      <div class="col-xs-12 col-sm-10 col-md-11">
+        <div class="embed-responsive embed-responsive-4by3">
+          {{> gallery}}
+        </div>
+      </div>''', ctx)
+  elif p['type'] == 'page' and p['name'] == 'Contacts':
+    content = pystache.Renderer(file_encoding='UTF8').render('{{> contacts}}', ctx);
+  else:
+    content = '<img src="http://bvsd.org/health/PublishingImages/under-construction-icon-small1.gif">'
+
+  ctx.update({'content': content})
+  pg = pystache.Renderer(file_encoding='UTF8').render('{{> base}}', ctx)
+  print p['link']+'.html'
+  with open(p['link']+'.html','w') as f:
     f.write(pg.encode('UTF8'))
